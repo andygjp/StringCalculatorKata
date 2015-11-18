0a012d4c1fb139fd99a0eef3aa583ed4ae30d18d
Andrew Pattison <andygjp@hotmail.com>
2015-11-18 12:45:58 +0000

    Added the first case - failing.

---
+++ StringCalculator/StringCalculator.Tests/Tests.cs
@@ -0,0 +1,24 @@
+﻿namespace StringCalculator.Tests
+{
+    using FluentAssertions;
+    using Xunit;
+
+    public class When_I_supply_blank_input
+    {
+        [Fact]
+        public void It_should_return_zero()
+        {
+            var sut = new StringCalculator();
+            int actual = sut.Add("");
+            actual.Should().Be(0);
+        }
+    }
+
+    public class StringCalculator
+    {
+        public int Add(string input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
